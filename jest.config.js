979bd48227c6ac6eacdae31bc63b4c0c8f20418d
Tomasz Wojtkiewicz <222616@edu.p.lodz.pl>
2022-10-28 17:40:36 +0200

    Testing works again

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,7 @@
 module.exports = {
-  preset: "react-native",
+  preset: "jest-expo",
+  transformIgnorePatterns: [
+    "node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)"
+  ],
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
 };
