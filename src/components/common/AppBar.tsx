6e86d66a9f079a7e69b7d5b29d18f0d919e80037
Tomasz Wojtkiewicz <222616@edu.p.lodz.pl>
2022-12-02 16:43:33 +0100

    Changed safeAreaTop color for IOS

--- src/components/common/AppBar.tsx
+++ src/components/common/AppBar.tsx
@@ -1,9 +1,17 @@
 import { useNavigation } from "@react-navigation/native";
-import {NativeStackNavigationProp} from "@react-navigation/native-stack";
-import {RootStackParamList} from "../../navigation/AppNavigation";
-import { StatusBar, Box, HStack, Image, Text, Icon, IconButton } from "native-base";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import { RootStackParamList } from "../../navigation/AppNavigation";
+import {
+  StatusBar,
+  Box,
+  HStack,
+  Image,
+  Text,
+  Icon,
+  IconButton,
+} from "native-base";
 import { MaterialIcons } from "@expo/vector-icons";
-import logo from '../../../assets/logo/logoMockWhite.png'
+import logo from "../../../assets/logo/logoMockWhite.png";
 import COLORS from "../../constants/colors";
 
 type AppBarProp = NativeStackNavigationProp<RootStackParamList>;
@@ -12,21 +20,49 @@ export default function AppBar() {
   const navigation = useNavigation<AppBarProp>();
   const handleAccountPress = () => {
     navigation.navigate("AccountSettings");
-  }
-    return <>
-        <StatusBar barStyle="light-content" />
-        <Box safeAreaTop bg="violet.600" />
-        <HStack bg={COLORS.blood} px="1" py="3" justifyContent="space-between" alignItems="center" w="100%">
-          <HStack alignItems="center">
-            <Image source={logo} size='xs' alt="Logo" marginLeft={2}/>
-            <Text color="white" fontSize="24" fontWeight="bold" marginLeft={4}>
-              Off-road
-            </Text>
-          </HStack>
-          <HStack>
-            <IconButton icon={<Icon as={MaterialIcons} name="account-circle" size="xl" color="white" />}  onPress={handleAccountPress}/>
-            <IconButton icon={<Icon as={MaterialIcons} name="more-vert" size="xl" color="white" />} />
-          </HStack>
+  };
+  return (
+    <>
+      <StatusBar barStyle="light-content" />
+      <Box safeAreaTop bg={COLORS.blood} />
+      <HStack
+        bg={COLORS.blood}
+        px="1"
+        py="3"
+        justifyContent="space-between"
+        alignItems="center"
+        w="100%"
+      >
+        <HStack alignItems="center">
+          <Image source={logo} size="xs" alt="Logo" marginLeft={2} />
+          <Text color="white" fontSize="24" fontWeight="bold" marginLeft={4}>
+            Off-road
+          </Text>
         </HStack>
-      </>;
-  }
+        <HStack>
+          <IconButton
+            icon={
+              <Icon
+                as={MaterialIcons}
+                name="account-circle"
+                size="xl"
+                color="white"
+              />
+            }
+            onPress={handleAccountPress}
+          />
+          <IconButton
+            icon={
+              <Icon
+                as={MaterialIcons}
+                name="more-vert"
+                size="xl"
+                color="white"
+              />
+            }
+          />
+        </HStack>
+      </HStack>
+    </>
+  );
+}
