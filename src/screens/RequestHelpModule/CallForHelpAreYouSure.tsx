e84b723ed4d7bdaf8b2964a204b6e49761c87c18
Tomasz Wojtkiewicz <222616@edu.p.lodz.pl>
2022-12-02 16:31:24 +0100

    Prośba o pomoc - weryfikacja maila

--- src/screens/RequestHelpModule/CallForHelpAreYouSure.tsx
+++ src/screens/RequestHelpModule/CallForHelpAreYouSure.tsx
@@ -12,64 +12,63 @@ import { RootStackParamList } from "../../navigation/AppNavigation";
 import { getAuth } from "firebase/auth";
 
 const isEmailCorrect = (email: string): boolean => {
-	return email == "test"; // TODO: change when navigation between screens is implemented
-	// return email == getAuth().currentUser?.email;
+  return email == getAuth().currentUser?.email;
 };
 
 const CallForHelpAreYouSure = () => {
-	const [email, setEmail] = useState("");
-	const [okButtonStyle, setOkButtonStyle] = useState(styles.button);
-	const navigation =
-		useNavigation<NativeStackNavigationProp<RootStackParamList>>();
+  const [email, setEmail] = useState("");
+  const [okButtonStyle, setOkButtonStyle] = useState(styles.button);
+  const navigation =
+    useNavigation<NativeStackNavigationProp<RootStackParamList>>();
 
-	return (
-		<View style={styles.wrapper}>
-			<Center>
-				<WarningOutlineIcon style={styles.warningIcon} />
-				<Text style={styles.warningHeader}>Warning</Text>
-			</Center>
+  return (
+    <View style={styles.wrapper}>
+      <Center>
+        <WarningOutlineIcon style={styles.warningIcon} />
+        <Text style={styles.warningHeader}>Warning</Text>
+      </Center>
 
-			<Center>
-				<Text style={styles.paragraph}>
-					You are about to ask for the help of every user in 30km radius. This
-					action can be performed about once every hour. Abusing this action
-					WILL get you banned. Write your email to accept this operation.
-				</Text>
-				<CustomInput
-					state={email}
-					setState={(input: string) => {
-						setEmail(input);
-						setOkButtonStyle(
-							isEmailCorrect(input)
-								? styles.buttonWhenEmailCorrect
-								: styles.button
-						);
-					}}
-					placeholder="Email"
-					icon={<Feather name="at-sign" color={COLORS.blood} />}
-				/>
-			</Center>
+      <Center>
+        <Text style={styles.paragraph}>
+          You are about to ask for the help of every user in 30km radius. This
+          action can be performed about once every hour. Abusing this action
+          WILL get you banned. Write your email to accept this operation.
+        </Text>
+        <CustomInput
+          state={email}
+          setState={(input: string) => {
+            setEmail(input);
+            setOkButtonStyle(
+              isEmailCorrect(input)
+                ? styles.buttonWhenEmailCorrect
+                : styles.button
+            );
+          }}
+          placeholder="Email"
+          icon={<Feather name="at-sign" color={COLORS.blood} />}
+        />
+      </Center>
 
-			<View>
-				<View style={okButtonStyle} testID={"opacityView"}>
-					<CustomButton
-						margin={0}
-						text="OK, HELP ME!"
-						clickHandler={() => {
-							if (!isEmailCorrect(email)) return;
-							navigation.navigate("CallForHelp");
-						}}
-					/>
-				</View>
-				<CustomButton
-					text="GO BACK"
-					clickHandler={() => {
-						navigation.navigate("MainScreen");
-					}}
-				/>
-			</View>
-		</View>
-	);
+      <View>
+        <View style={okButtonStyle} testID={"opacityView"}>
+          <CustomButton
+            margin={0}
+            text="OK, HELP ME!"
+            clickHandler={() => {
+              if (!isEmailCorrect(email)) return;
+              navigation.navigate("CallForHelp");
+            }}
+          />
+        </View>
+        <CustomButton
+          text="GO BACK"
+          clickHandler={() => {
+            navigation.navigate("MainScreen");
+          }}
+        />
+      </View>
+    </View>
+  );
 };
 
 export default CallForHelpAreYouSure;
