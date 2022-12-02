f5db83010fb27f850f1d8165629ea06317086711
Tomasz Wojtkiewicz <222616@edu.p.lodz.pl>
2022-12-02 18:30:03 +0100

    Dodanie inputu do ustawienia zasięgu pomocy, Komunikat błędu przy błędnych danych do zapisu

--- src/screens/AccountSettings/AccountSettings.tsx
+++ src/screens/AccountSettings/AccountSettings.tsx
@@ -1,9 +1,9 @@
-import { useState } from "react";
-import { View, Image, Text } from "react-native";
-import { VStack, Center } from "native-base";
-import { Feather, Foundation, Entypo } from "@expo/vector-icons";
+import React, { useState } from "react";
+import { Dimensions, Text, View } from "react-native";
+import { Center, Select, VStack, CheckIcon, Box, Icon } from "native-base";
+import { Entypo, Feather, MaterialCommunityIcons } from "@expo/vector-icons";
 
-import { getAuth, updateEmail } from "firebase/auth";
+import { getAuth } from "firebase/auth";
 import { useNavigation } from "@react-navigation/native";
 import { NativeStackNavigationProp } from "@react-navigation/native-stack";
 import { RootStackParamList } from "../../navigation/AppNavigation";
@@ -12,109 +12,143 @@ import CustomInput from "../../components/common/CustomInput";
 import CustomButton from "../../components/common/CustomButton";
 import styles from "./styles/AccountSettings.styles";
 import COLORS from "../../constants/colors";
-import { validateEmail, validatePassword } from "../../utils/validators";
 import HomeAppBar from "../../components/common/HomeAppBar";
 import { addUserData } from "../../utils/firestore";
+import { validateRange } from "../../utils/validators";
 
 type AccountSettingsProp = NativeStackNavigationProp<RootStackParamList>;
 
-export default function AccountSettings(props: any) {
-	const [phone, setPhone] = useState("");
-	const [address, setAddress] = useState("");
-	const [saveSuccess, setSaveSuccess] = useState(false);
-	const [saveError, setSaveError] = useState(false);
-	const [saveErrorMessage, setSaveErrorMessage] = useState("");
+export default function AccountSettings() {
+  const [phone, setPhone] = useState("");
+  const [address, setAddress] = useState("");
+  const [saveSuccess, setSaveSuccess] = useState(false);
+  const [saveError, setSaveError] = useState(false);
+  const [saveErrorMessage, setSaveErrorMessage] = useState("");
+  const [range, setRange] = useState("");
 
-	const handlePhoneChange = (text: string) => {
-		setPhone(text);
-	};
+  const handleRangeChange = (text: string) => {
+    setRange(text);
+  };
 
-	const handleAddressChange = (text: string) => {
-		setAddress(text);
-	};
+  const handlePhoneChange = (text: string) => {
+    setPhone(text);
+  };
 
-	const navigation = useNavigation<AccountSettingsProp>();
-	const handleChangePasswordPress = () => {
-		navigation.navigate("ChangePassword");
-	};
-	const handleDeleteAccountPress = () => {
-		navigation.navigate("DeleteAccount");
-	};
+  const handleAddressChange = (text: string) => {
+    setAddress(text);
+  };
 
-	const handleChangeEmailPress = () => {
-		navigation.navigate("ChangeEmail");
-	};
+  const navigation = useNavigation<AccountSettingsProp>();
+  const handleChangePasswordPress = () => {
+    navigation.navigate("ChangePassword");
+  };
+  const handleDeleteAccountPress = () => {
+    navigation.navigate("DeleteAccount");
+  };
 
-	const handleSaveInfo = () => {
-		const auth = getAuth();
-		setSaveSuccess(false);
-		setSaveError(false);
-		if (auth.currentUser && auth.currentUser.email) {
-			addUserData(auth.currentUser.email, address, phone)
-				.then((res) => {
-					setSaveSuccess(true);
-				})
-				.catch((error) => {
-					console.log(error);
-					setSaveError(true);
-					setSaveErrorMessage(error.message);
-				});
-		}
-	};
+  const handleChangeEmailPress = () => {
+    navigation.navigate("ChangeEmail");
+  };
 
-	return (
-		<View style={styles.container}>
-			<VStack>
-				<HomeAppBar text="Profile" />
-				<Center marginTop="5%">
-					<Text style={styles.headerText}>CONTACT INFO</Text>
-				</Center>
-				<Center marginTop="5%">
-					<CustomInput
-						state={phone}
-						setState={handlePhoneChange}
-						placeholder="Phone"
-						icon={<Entypo name="phone" color={COLORS.blood} />}
-						isContentInvalid={false}
-						margin={2}
-					/>
-					<CustomInput
-						state={address}
-						setState={handleAddressChange}
-						placeholder="Address"
-						icon={<Entypo name="home" color={COLORS.blood} />}
-						isContentInvalid={false}
-						margin={2}
-					/>
-				</Center>
-				<Center>
-					{saveError && <Text style={styles.textBold}>{saveErrorMessage}</Text>}
-					{saveSuccess && <Text style={styles.textBold}>Saved user info</Text>}
-				</Center>
-				<Center>
-					<CustomButton text="SAVE" clickHandler={handleSaveInfo} />
-				</Center>
-				<Center marginTop="5%">
-					<Text style={styles.headerText}>DANGER ZONE</Text>
-				</Center>
-				<Center marginTop="3%">
-					<CustomButton
-						text="CHANGE E-MAIL"
-						clickHandler={handleChangeEmailPress}
-						margin={3}
-					/>
-					<CustomButton
-						text="CHANGE PASSWORD"
-						clickHandler={handleChangePasswordPress}
-						margin={3}
-					/>
-					<CustomButton
-						text="DELETE ACCOUNT"
-						clickHandler={handleDeleteAccountPress}
-						margin={3}
-					/>
-				</Center>
-			</VStack>
-		</View>
-	);
+  const handleSaveInfo = () => {
+    const auth = getAuth();
+    let errorMessage = "";
+    if (phone === "") {
+      errorMessage = "Can not save empty phone number!";
+    } else if (address === "") {
+      errorMessage = "Can not save empty address!";
+    } else if (range === "") {
+      errorMessage = "Can not save empty range!";
+    } else if (!validateRange(range)) {
+      errorMessage = "Incorrect range!";
+    }
+    // TODO: add address and phone number validation
+    if (errorMessage != "") {
+      setSaveError(true);
+      setSaveErrorMessage(errorMessage);
+      return;
+    }
+    setSaveSuccess(false);
+    setSaveError(false);
+    if (auth.currentUser && auth.currentUser.email) {
+      addUserData(auth.currentUser.email, address, phone)
+        .then((res) => {
+          setSaveSuccess(true);
+        })
+        .catch((error) => {
+          console.log(error);
+          setSaveError(true);
+          setSaveErrorMessage(error.message);
+        });
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <VStack>
+        <HomeAppBar text="Profile" />
+        <Center marginTop="5%">
+          <Text style={styles.headerText}>USER INFO</Text>
+        </Center>
+        <Center marginTop="5%">
+          <CustomInput
+            state={phone}
+            setState={handlePhoneChange}
+            placeholder="Phone"
+            icon={<Entypo name="phone" color={COLORS.blood} />}
+            isContentInvalid={false}
+            margin={2}
+          />
+          <CustomInput
+            state={address}
+            setState={handleAddressChange}
+            placeholder="Address"
+            icon={<Entypo name="home" color={COLORS.blood} />}
+            isContentInvalid={false}
+            margin={2}
+          />
+          <CustomInput
+            state={range}
+            setState={handleRangeChange}
+            placeholder="Help range (km)"
+            icon={
+              <MaterialCommunityIcons
+                name="map-marker-distance"
+                color={COLORS.blood}
+              />
+            }
+            isContentInvalid={false}
+            margin={2}
+          />
+        </Center>
+        <Center>
+          {saveError && <Text style={styles.textBold}>{saveErrorMessage}</Text>}
+          {saveSuccess && <Text style={styles.textBold}>Saved user info</Text>}
+        </Center>
+        <Center>
+          <CustomButton text="SAVE" clickHandler={handleSaveInfo} />
+        </Center>
+        <Center marginTop="5%">
+          <Text style={styles.headerText}>DANGER ZONE</Text>
+        </Center>
+        <Center marginTop="3%">
+          <CustomButton
+            text="CHANGE E-MAIL"
+            clickHandler={handleChangeEmailPress}
+            margin={3}
+          />
+          <CustomButton
+            text="CHANGE PASSWORD"
+            clickHandler={handleChangePasswordPress}
+            margin={3}
+          />
+          <CustomButton
+            text="DELETE ACCOUNT"
+            clickHandler={handleDeleteAccountPress}
+            margin={3}
+          />
+        </Center>
+      </VStack>
+    </View>
+  );
 }
--- src/utils/validators.ts
+++ src/utils/validators.ts
@@ -1,17 +1,24 @@
 export const validateEmail = (email: string) => {
-  const re = /^[A-Za-z0-9]+@[A-Za-z0-9]+\.[A-Za-z0-9]+/
+  const re = /^[A-Za-z0-9]+@[A-Za-z0-9]+\.[A-Za-z0-9]+/;
 
   return re.test(email);
-}
+};
 
 export const validateUsername = (username: string) => {
   return username.length > 4;
-}
+};
 
 export const validatePassword = (password: string) => {
   return password.length > 5;
-}
+};
 
-export const validateRepeatedPassword = (password: string, repeatedPassword: string) => {
+export const validateRepeatedPassword = (
+  password: string,
+  repeatedPassword: string
+) => {
   return password === repeatedPassword;
-}
+};
+
+export const validateRange = (range: string) => {
+  return /[1-9][0-9]*"km"?/.test(range) || range === "0";
+};
